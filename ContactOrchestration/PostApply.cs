958ef9fe9fa036e7f27459b2e78cff782384c8ad
malindasuhash <malinda.suhash@gmail.com>
2026-02-22 16:07:41 +0000

    Added post apply

---
+++ ContactOrchestration/PostApply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactOrchestration
+{
+    internal class PostApply
+    {
+        public async Task Run(RuntimeInfo runtimeInfo)
+        {
+            // Retrieve list of legal entities associated
+            // with the contact and touch them.
+            return;
+        }
+    }
+}
