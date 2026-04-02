96bd7a98cea1270031061dfea97312d1f4026802
malindasuhash <malinda.suhash@gmail.com>
2026-04-02 08:11:06 +0100

    Completed Contact evaluation steps. Next, I am considering to implement apply and post apply steps.

--- Contact.Orchestration.Svc/Services/ContactEvaluator.cs
+++ Contact.Orchestration.Svc/Services/ContactEvaluator.cs
@@ -1,11 +1,10 @@
-﻿
-using Contact.Orchestration.Svc.Contracts;
+﻿using Contact.Orchestration.Svc.Contracts;
 using Contact.Orchestration.Svc.Model;
 using StateManagment.Models;
 
 namespace Contact.Orchestration.Svc.Services
 {
-    public class ContactEvaluator : IEvaluator 
+    public class ContactEvaluator : IEvaluator
     {
         private readonly ISender sender;
 
@@ -15,9 +14,38 @@ namespace Contact.Orchestration.Svc.Services
         }
         public async Task Evaluate(RequestData requestData, CancellationToken stoppingToken)
         {
-            var envelope = OrchestrationEnvelop.Create(EntityName.Contact, requestData.EntityId, requestData.CustomerId, requestData.SubmittedVersion, RuntimeStatus.EVALUATION_STARTED);
+            EvaluationStarted(requestData);
 
-            await sender.SendAsync(envelope, requestData.CorellationId);
+            // Simulate evaluation process
+            if (requestData is not ContactRequestData contactRequest) return;
+
+            var feedbacks = new List<Feedback>();
+
+            // Checks to perform:
+            if (contactRequest.Submitted.FirstName == null || contactRequest.Submitted.FirstName.StartsWith("test"))
+            {
+                feedbacks.Add(new Feedback() { Type = FeedbackType.Error, Key = "FirstName", Value = "InvalidFirstName" });
+            }
+
+            if (contactRequest.Submitted.PostalAddress == null)
+            {
+                feedbacks.Add(new Feedback() { Type = FeedbackType.Error, Key = "PostalAddress", Value = "PostalAddressMissing" });
+            }
+
+            if (feedbacks.Count == 0)
+            {
+                await sender.SendAsync(OrchestrationEnvelop.Create(EntityName.Contact, requestData.EntityId, requestData.CustomerId, requestData.SubmittedVersion, RuntimeStatus.EVALUATION_COMPLETED), requestData.CorellationId);
+            }
+            else
+            {
+                await sender.SendAsync(OrchestrationEnvelop.Create(EntityName.Contact, requestData.EntityId, requestData.CustomerId, requestData.SubmittedVersion, RuntimeStatus.EVALUATION_INCOMPLETE, feedbacks: feedbacks.ToArray()), requestData.CorellationId);
+            }
+        }
+
+        private void EvaluationStarted(RequestData requestData)
+        {
+            var envelope = OrchestrationEnvelop.Create(EntityName.Contact, requestData.EntityId, requestData.CustomerId, requestData.SubmittedVersion, RuntimeStatus.EVALUATION_STARTED);
+            sender.SendAsync(envelope, requestData.CorellationId);
         }
     }
 }
