5b61da578a5adfa14eb1cf29e68fd8758f14625a
malindasuhash <malinda.suhash@gmail.com>
2026-04-05 12:01:02 +0100

    I have attempted to integrate API first design principles. Appears to be working so far. Key point is defining the contract very clearly.

---
+++ ContractFirstDesign/Examples/DtoUsageExample.cs
@@ -0,0 +1,67 @@
+using ContractFirstDesign;
+
+namespace ContractFirstDesign.Examples;
+
+public class DtoUsageExample
+{
+    public CreateLegalEntity BuildNewLegalEntity()
+    {
+        return new CreateLegalEntity
+        {
+            Name = "Acme Ltd",
+            Business_email = "info@acme.com",
+            Business_type = "Retail",
+            Company_registration_no = "12345678",
+            Trading_name = "Acme Trading",
+            Merchant_category_code = "5411",
+            Standard_industry_classification = "4711",
+            Turnover_per_annum = 500000,
+            Card_turnover_per_annum = 200000,
+            Maximum_transaction_value = 5000,
+            Vat_registration_status = VatRegistrationStatus.Registered,
+            Vat_registration_no = "GB123456789",
+            Date_business_created = "2020-06-15",
+            Date_trading_started = "2020-07-01",
+            Persons_with_control =
+            [
+                new PersonWithControl
+                {
+                    Person = new Person
+                    {
+                        Title = "Mr",
+                        First_name = "John",
+                        Last_name = "Smith",
+                        Date_of_birth = "1985-03-20",
+                        Nationality = "GB",
+                        Address = new Address
+                        {
+                            Line1 = "10 High Street",
+                            Locality = "London",
+                            Region = "Greater London",
+                            Code = "EC1A 1BB",
+                            Country = "GB"
+                        }
+                    },
+                    Control_types = [PersonControlType.Director, PersonControlType.Shareholder]
+                }
+            ],
+            Registered_addresses =
+            [
+                new RegisteredAddress
+                {
+                    Current = true,
+                    Date_from = "2020-06-15",
+                    Address = new Address
+                    {
+                        Name = "Acme House",
+                        Line1 = "1 Business Park",
+                        Locality = "Manchester",
+                        Region = "Greater Manchester",
+                        Code = "M1 1AA",
+                        Country = "GB"
+                    }
+                }
+            ]
+        };
+    }
+}
