63a6929caca430661df129cc791660400456aa16
Malinda Suhash <malinda.suhash@live.com>
2026-05-17 07:30:09 +0100

    Minor optimisation to header validation logic.

--- Api/Correlation/CorrelationHeadersMiddleware.cs
+++ Api/Correlation/CorrelationHeadersMiddleware.cs
@@ -26,22 +26,40 @@ namespace Api.Correlation
             if (!context.Request.Headers.TryGetValue(CorrelationHeader, out var correlationValues) || string.IsNullOrWhiteSpace(correlationValues))
             {
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsync($"Missing required header: {CorrelationHeader}");
+
+                var errorCorrelationId = CreateRfcClientError($"Missing required header: {CorrelationHeader}", "#missing-header", new Dictionary<string, object>
+                    {
+                        { "missingHeader", CorrelationHeader }
+                    });
+
+                context.Response.ContentType = "application/problem+json";
+                await context.Response.WriteAsJsonAsync(errorCorrelationId);
                 return;
             }
 
             if (!context.Request.Headers.TryGetValue(ClientHeader, out var clientValues) || string.IsNullOrWhiteSpace(clientValues))
             {
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsync($"Missing required header: {ClientHeader}");
+                var errorClientId = CreateRfcClientError($"Missing required header: {ClientHeader}", "#missing-header", new Dictionary<string, object>
+                    {
+                        { "missingHeader", ClientHeader }
+                    });
+
+                context.Response.ContentType = "application/problem+json";
+                await context.Response.WriteAsJsonAsync(errorClientId);
                 return;
             }
 
             if (!Guid.TryParse(correlationValues.ToString(), out var correlationId))
             {
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsync($"Invalid GUID in header: {CorrelationHeader}");
-                return;
+                var errorGuid = CreateRfcClientError($"Formatting error: {ClientHeader}", "#header-format", new Dictionary<string, object>
+                    {
+                        { "incorrect-format", "GUID required" }
+                    });
+
+                context.Response.ContentType = "application/problem+json";
+                await context.Response.WriteAsJsonAsync(errorGuid);
             }
 
             var clientId = clientValues.ToString();
@@ -57,6 +75,20 @@ namespace Api.Correlation
             {
                 await _next(context);
             }
+        }
+
+        private static ApiContract.Rfc7807 CreateRfcClientError(string message, string fragment, Dictionary<string, object> parameters)
+        {
+            var rfcError = new ApiContract.Rfc7807()
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Title = "Bad Request",
+                Detail = message,
+                Type = fragment,
+                Parameters = parameters
+            };
+
+            return rfcError;
         }
     }
 }
